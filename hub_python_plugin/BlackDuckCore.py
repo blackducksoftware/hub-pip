ac0300e1ae21fe2e7b4beb697c28151631a77745
Jake Mathews <mathewsj2@wit.edu>
2017-03-20 07:52:05 -0400

    Moved core functionality to seperate file

---
+++ hub_python_plugin/BlackDuckCore.py
@@ -0,0 +1,58 @@
+import pip
+import pkg_resources
+import distutils
+import Package
+
+def get_setup_dependencies(package):
+    project_requirement = pkg_resources.Requirement.parse(package)
+
+    environment = pkg_resources.Environment(
+        distutils.sysconfig.get_python_lib(),
+        platform=None,
+        python=None
+    )
+
+    dependencies = pkg_resources.working_set.resolve(
+        [project_requirement], env=environment
+    )
+
+    #dependencies = pkg_resources.working_set.find(project_requirement)
+    return dependencies
+
+def get_dependencies(pkg):
+    dependencies = []
+    for dependency in pkg.requires():
+        pkg = get_best(dependency)
+        if pkg:
+            pkg_dependencies = get_dependencies(pkg)
+            package = Package.make_package(
+                pkg.key, pkg.version, pkg_dependencies)
+            dependencies.append(package)
+    return dependencies
+
+
+def get_best(dependency):  # Needs some work to check for multiple packages
+    installed = pip.get_installed_distributions(
+        local_only=False, user_only=False)
+
+    for pkg in installed:
+        if pkg.key == dependency.key:
+            return pkg
+    print("No match found for: " + dependency.key)
+    return None
+
+
+def render_tree(root, layer=0):
+    result = root.name + "==" + root.version
+    dependencies = sorted(root.dependencies, key=lambda x: x.name)
+    for dependency in dependencies:
+        result += "\n" + (" " * 4 * layer)
+        result += render_tree(dependency, layer + 1)
+    return result
+
+def render_flat(pkgs):
+    result = ""
+    for pkg in pkgs:
+        full_pkg = get_best(pkg)
+        result += full_pkg.key + "==" + full_pkg.version + "\n"
+    return result
