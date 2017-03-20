27f79b171f374ae9a51d55a25c9af42bd394cf95
Jake Mathews <mathewsj2@wit.edu>
2017-03-20 07:35:41 -0400

    Added tree command

--- hub_python_plugin/BlackDuckPlugin.py
+++ hub_python_plugin/BlackDuckPlugin.py
@@ -15,6 +15,7 @@ class BlackDuckCommand(Command):
     user_options = [
         ("config-path=", "c", "Path to Black Duck Configuration file"),
         ("flat-list=", "f", "Generate flat list"),
+        ("tree-list=", "t", "Generate tree list"),
     ]
 
     def initialize_options(self):
@@ -22,6 +23,7 @@ class BlackDuckCommand(Command):
         # Each user option must be listed here with their default value.
         self.config_path = None
         self.flat_list = False
+        self.tree_list = False
 
     def finalize_options(self):
         """Post-process options."""
@@ -33,14 +35,23 @@ class BlackDuckCommand(Command):
         """Run command."""
 
         flat = self.flat_list
+        tree = self.tree_list
+
         project_av = self.distribution.get_name() + "==" + self.distribution.get_version()
-        if(flat):
-            pkgs = get_raw_dependencies(project_av)
-            pkg = pkgs.pop(0)
+
+        if(tree):
+            pkgs = get_setup_dependencies(project_av)
+            pkg = pkgs.pop(0)  # The first dependency is itself
             pkg_dependencies = get_dependencies(pkg)
             root = Package.make_package(pkg.key, pkg.version, pkg_dependencies)
-            print(render_tree(root))
+            print(render_tree(root, 1))
 
+        if(flat):
+            pkgs = get_setup_dependencies(project_av)
+            pkg = pkgs.pop(0)  # The first dependency is itself
+            pkgs = list(set(pkgs)) # Remove duplicates
+            pkgs.sort(key = lambda x: x.key)
+            print(render_flat(pkgs))
 
 def get_dependencies(pkg):
     dependencies = []
@@ -48,14 +59,16 @@ def get_dependencies(pkg):
         pkg = get_best(dependency)
         if pkg:
             pkg_dependencies = get_dependencies(pkg)
-            package = Package.make_package(pkg.key, pkg.version, pkg_dependencies)
+            package = Package.make_package(
+                pkg.key, pkg.version, pkg_dependencies)
             dependencies.append(package)
     return dependencies
 
 
-def get_best(dependency):
+def get_best(dependency):  # Needs some work to check for multiple packages
     installed = pip.get_installed_distributions(
         local_only=False, user_only=False)
+
     for pkg in installed:
         if pkg.key == dependency.key:
             return pkg
@@ -65,12 +78,21 @@ def get_best(dependency):
 
 def render_tree(root, layer=0):
     result = root.name + "==" + root.version
-    for dependency in root.dependencies:
+    dependencies = sorted(root.dependencies, key=lambda x: x.name)
+    for dependency in dependencies:
         result += "\n" + (" " * 4 * layer)
         result += render_tree(dependency, layer + 1)
     return result
 
-def get_raw_dependencies(package):
+def render_flat(pkgs):
+    result = ""
+    for pkg in pkgs:
+        full_pkg = get_best(pkg)
+        result += full_pkg.key + "==" + full_pkg.version + "\n"
+    return result
+
+
+def get_setup_dependencies(package):
     project_requirement = pkg_resources.Requirement.parse(package)
 
     environment = pkg_resources.Environment(
